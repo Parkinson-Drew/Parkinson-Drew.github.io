8f577c063cd6b7ffc26fa81c57a4fdf77d6d3369
Parkinson-Drew <parkidr@gmail.com>
2020-05-16 16:54:47 -0700

    updates to match weather site plan

--- lesson4/js/site.js
+++ lesson4/js/site.js
@@ -1,36 +1,27 @@
 function GetFileInfo() {
     var message = "";
 
-    // if (document.fileModifiedDate) {
-    //     message += "Last updated: " + document.fileModifiedDate;
-    // }
-    // if (document.lastModified) {
-    //     message += "Last updated: " + document.lastModified;
-    // }
-    // if (document.fileUpdatedDate) {
-    //     message += "Last updated: " + document.fileUpdatedDate;
-    // }
+    var month_names = ["January", "February", "March",
+        "April", "May", "June", "July", "August", "September",
+        "October", "November", "December"
+    ];
 
-    // var info = document.getElementById("updatedDate");
-    // info.textContent = message;
-    var m_names = ["January", "February", "March", 
-    "April", "May", "June", "July", "August", "September", 
-    "October", "November", "December"];
-    
-    var d_names = ["Sunday","Monday", "Tuesday", "Wednesday", 
-    "Thursday", "Friday", "Saturday"];
-    
-    var myDate = new Date();
-    var curr_date = myDate.getDate();
-    var curr_month = myDate.getMonth();
-    var curr_day  = myDate.getDay();
-    var curr_year = myDate.getFullYear();
-    message = (d_names[curr_day] + ", " +  curr_day + " " + m_names[curr_month] + " " +curr_year);
+    var day_names = ["Sunday", "Monday", "Tuesday", "Wednesday",
+        "Thursday", "Friday", "Saturday"
+    ];
+    // idea inspired from a stackoverflow article:
+    // https://stackoverflow.com/questions/19583312/display-day-of-the-week-with-javascript-date
+    let myDate = new Date();
+    let current_date = myDate.getDate();
+    let current_month = myDate.getMonth();
+    let current_day = myDate.getDay();
+    let current_year = myDate.getFullYear();
+    message = (day_names[current_day] + ", " + current_day + " " + month_names[current_month] + " " + current_year);
 
-    var date = new Date();
-    var currYear = document.getElementById("currentDate");
+    let date = new Date();
+    let currYear = document.getElementById("currentDate");
     // message += date.getDate();
-    currYear.textContent = message;  
+    currYear.textContent = message;
 }
 
 function toggleMenu() {
@@ -38,4 +29,4 @@ function toggleMenu() {
         .getElementsByClassName("flex-nav")[0]
         .classList.toggle("responsive");
 
-  }
+}
