27457369da21a2dd7e7261027026653ada9c8844
Parkinson-Drew <parkidr@gmail.com>
2020-05-28 00:52:02 -0700

    updates windchill logic

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -9,7 +9,7 @@ var windchill = 35.74 + (0.6215 * aveTemp) - (35.75 * s) +	(0.4275 * aveTemp * s
 
 windchill = Math.round(windchill) + "&deg;F";
 
-if (windSpeed < 4.8 && aveTemp > 50)
+if (windSpeed < 4.8 || aveTemp > 50)
 {
     document.getElementById("windchill").innerHTML = "N/A"    
 }
